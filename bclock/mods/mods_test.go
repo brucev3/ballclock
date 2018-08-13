9c9a163dbc186caa8ef95a896e116d3008886fdd
brucev3 <brucev@gmail.com>
2018-08-13 08:45:47 -0600

    fix unit test error strings, add unit tests for ComputeDays

--- bclock/mods/mods_test.go
+++ bclock/mods/mods_test.go
@@ -6,31 +6,75 @@ import (
 
 func TestIsInitialOrdering1(t *testing.T) {
 	var initial = "{\"Min\":[],\"FiveMin\":[],\"Hour\":[],\"Main\":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]}"
-	var new1 = "{\"Min\":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],\"FiveMin\":[],\"Hour\":[],\"Main\":[]}"
+	var mismatch = "{\"Min\":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],\"FiveMin\":[],\"Hour\":[],\"Main\":[]}"
 
-	resnew1 := IsInitialOrdering(initial, new1)
-		if resnew1 {
-			//t.Error("Result is incorrect, got: %t, want: %t.", resnew, false)
-			t.Error("Result is incorrect, got: true, want: false.")
-		}
+	result := IsInitialOrdering(initial, mismatch)
+	if result {
+		t.Errorf("Result is incorrect, got: %t, want: %t.", result, false)
 	}
+}
 
 func TestIsInitialOrdering2(t *testing.T) {
 	var initial = "{\"Min\":[],\"FiveMin\":[],\"Hour\":[],\"Main\":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]}"
-	var new2 = "{\"Min\":[1,2,3],\"FiveMin\":[],\"Hour\":[],\"Main\":[4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]}"
+	var mismatch = "{\"Min\":[1,2,3],\"FiveMin\":[],\"Hour\":[],\"Main\":[4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]}"
 
-	resnew2 := IsInitialOrdering(initial, new2)
-		if resnew2 {
-			t.Error("Result is incorrect, got: true, want: false.")
-		}
+	result := IsInitialOrdering(initial, mismatch)
+	if result {
+		t.Errorf("Result is incorrect, got: %t, want: %t.", result, false)
 	}
+}
 
 func TestIsInitialOrdering3(t *testing.T) {
 	var initial = "{\"Min\":[],\"FiveMin\":[],\"Hour\":[],\"Main\":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]}"
 	var match = "{\"Min\":[],\"FiveMin\":[],\"Hour\":[],\"Main\":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]}"
 
-	resmatch := IsInitialOrdering(initial, match)
-		if !resmatch {
-			t.Error("Result is incorrect, got: false, want: true.")
-		}
+	result := IsInitialOrdering(initial, match)
+	if !result {
+		t.Errorf("Result is incorrect, got: %t, want: %t.", result, true)
 	}
+}
+
+func TestComputeDays1(t *testing.T) {
+	var minutes = 1430
+
+	result := ComputeDays(minutes)
+	if result != 0 {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, 0)
+	}
+}
+
+func TestComputeDays2(t *testing.T) {
+	var minutes = 1440
+
+	result := ComputeDays(minutes)
+	if result != 1 {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, 1)
+	}
+}
+
+func TestComputeDays3(t *testing.T) {
+	var minutes = 1445
+
+	result := ComputeDays(minutes)
+	if result != 1 {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, 1)
+	}
+}
+
+func TestComputeDays4(t *testing.T) {
+	var minutes = 2870
+
+	result := ComputeDays(minutes)
+	if result != 1 {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, 1)
+	}
+}
+
+func TestComputeDays5(t *testing.T) {
+	var minutes = 2885
+
+	result := ComputeDays(minutes)
+	if result != 2 {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, 2)
+	}
+}
